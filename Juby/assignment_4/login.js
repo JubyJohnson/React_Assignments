32508c9e5aafd14fca1fffe6efc48e3e20d7c7f7
JubyJohnson <51119107+JubyJohnson@users.noreply.github.com>
2023-02-12 14:53:20 +0530

    Add files via upload

---
+++ Juby/assignment_4/login.js
@@ -0,0 +1,22 @@
+function login() {
+    var userName = document.getElementById("username");
+    var passWord = document.getElementById("password");
+    if (userName.value == "" || passWord.value == "") {
+      alert("username/paaword cannot be blank");
+      return false;
+    } else if (password.value == null || password.value == "") {
+      alert("password cannot be blank");
+      return false;
+    } else {
+      if (password.value.length < 6) {
+        alert("password must contain more characters");
+        return false;
+      }
+      return true;
+    }
+  }
+  function cancel(){
+      alert("Please enter the data")
+      document.getElementById("username").value="";
+      document.getElementById("password").value="";
+  }
