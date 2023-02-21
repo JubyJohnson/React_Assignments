2ff51343d8caee35927cca27838e60b002179193
JubyJohnson <51119107+JubyJohnson@users.noreply.github.com>
2023-02-21 12:35:50 +0530

    Add files via upload

---
+++ Juby/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Example from "./components/funct-comp/Example";
+import ParentComponent from "./components/funct-comp/ParentComponent";
+
+function App() {
+  return (
+    <div className="App">
+   <Example />
+   <ParentComponent/>
+   </div>
+  );
+}
+
+export default App;
