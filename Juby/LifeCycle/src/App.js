4d058684e0ca7f877c971f43791aa73e3e8bbea4
JubyJohnson <51119107+JubyJohnson@users.noreply.github.com>
2023-03-02 23:27:11 +0530

    Add files via upload

---
+++ Juby/LifeCycle/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/funct-comp/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login Page </h1>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
